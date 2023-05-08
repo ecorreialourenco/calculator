7c14331493cf7c2e9b3b26ba9db3187cc87fc0ff
Emanuel Lourenço <dodgy6@gmail.com>
2023-05-08 11:18:34 +0100

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import Calculator from "./calculator/Calculator";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
-}
-
-export default App;
+export const App = () => (
+  <div className="App">
+    <Calculator />
+  </div>
+);
